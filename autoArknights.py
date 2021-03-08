25918187c7672efc6111a7928d8e6d05490fa3f5
liuzequan <675427976@qq.com>
2021-03-08 20:39:58 +0800

    update readme

--- autoArknights.py
+++ autoArknights.py
@@ -9,7 +9,7 @@ auto_setup(__file__)
 # dev = connect_device("Android://127.0.0.1:5037/emulator-5558")
 # begintion()
 links = [
-#         "Android://127.0.0.1:5037/emulator-5558",   # 貌似
+        "Android://127.0.0.1:5037/emulator-5558",   # 貌似
         "Android://127.0.0.1:5037/emulator-5556", # 天生
 #         "Android://127.0.0.1:5037/emulator-5558"    # 貌似
         ]
@@ -17,12 +17,13 @@ account = MyFangzhouZhushou()
 flag = False
 # flag = True
 if flag:
-    start_leidian()
+#     start_leidian()
     for link in links:
         dev = connect_device(link)
+        account.startZhuShou()
         account.startLoginFangZhou()
         account.after_login()
-sleeptime = 3600*3
+sleeptime = 3600*3.5
 # sleep(sleeptime)
 
 
@@ -33,6 +34,7 @@ for link in links:
     account.credit()
     account.recruition()
     account.tasktion()
+    sleep(3600*1.5)
 account.end()
 # sleep(3600*3)
 # for link in links:
